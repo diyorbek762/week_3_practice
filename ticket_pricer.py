416add50679f39b83fa7bcc892df5ebf6e874963
Diyorbek <dedamirzayevdiyorbek9@gmail.com>
2025-10-14 16:30:09 +0500

    master

---
+++ ticket_pricer.py
@@ -0,0 +1,13 @@
+
+age = int(input('Enter your age: '))
+print('--- Movie Ticket Pricer ---')
+print(f'Please enter your age: {age}')
+if age <= 12:
+    print("You fall into 'Children' category.")
+    print('Your ticket price is: 8$')
+elif age >= 65:
+    print(" You will fall into 'Senior' category.")
+    print('Your ticket price is: 10$')
+else:
+    print("You fall into 'Adult' category.")
+    print('Your ticket price is: 15$')
