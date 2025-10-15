6cef5818b9a8277812191b53839200180a37f50c
Diyorbek <dedamirzayevdiyorbek9@gmail.com>
2025-10-15 22:04:33 +0500

    Add multiple_game.py

--- multiple_game.py
+++ multiple_game.py
@@ -5,12 +5,12 @@
 # For numbers which are multiples of both three and five, print “FizzBuzz”.
 print('--- FizzBuzz Challenge (1-100) ---')
 for i in range(1, 101):
-    if i % 3 == 0:
+    if i % 15 == 0:
+        print('FizzBuzz')
+    elif i % 3 == 0:
         print('Fizz')
     elif i % 5 == 0:
         print('Buzz')
-    elif i % 15 == 0:
-        print('FizzBuzz')
     else:
         print(i)
 
